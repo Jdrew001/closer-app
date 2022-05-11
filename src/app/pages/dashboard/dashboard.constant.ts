e152eb721468022a15077ca9cf874b333b7ff9c2
Drew Atkison <dtatkison@gmail.com>
2022-05-11 17:09:18 -0500

    updated dashboard table

---
+++ src/app/pages/dashboard/dashboard.constant.ts
@@ -0,0 +1,142 @@
+export class DashboardConstant {
+    public static DATA = {
+        title: "Weekly Overview",
+        data: [
+            {
+                "subTitle": "Mar 25 - Mar 31",
+                "startDate": "03/25/2022",
+                "endDate": "03/31/2022",
+                "graphData": [
+                    {
+                        key: 'M',
+                        value: 20,
+                        "date": "03/25/2022"
+                    },
+                    {
+                        key: 'T',
+                        value: 50,
+                        "date": "03/26/2022"
+                    },
+                    {
+                        key: 'W',
+                        value: 85,
+                        "date": "03/27/2022"
+                    },
+                    {
+                        key: 'T',
+                        value: 40,
+                        "date": "03/28/2022"
+                    },
+                    {
+                        key: 'F',
+                        value: 55,
+                        "date": "03/29/2022"
+                    },
+                    {
+                        key: 'S',
+                        value: 20,
+                        "date": "03/30/2022"
+                    },
+                    {
+                        key: 'S',
+                        value: 100,
+                        "date": "03/31/2022"
+                    },
+                ],
+                selected: false,
+                defaultSelected: "M"
+            },
+            {
+                "subTitle": "Apr 1 - Apr 7",
+                "startDate": "04/01/2022",
+                "endDate": "04/07/2022",
+                "graphData": [
+                    {
+                        key: 'S',
+                        value: 100,
+                        "date": "04/01/2022"
+                    },
+                    {
+                        key: 'M',
+                        value: 20,
+                        "date": "04/02/2022"
+                    },
+                    {
+                        key: 'T',
+                        value: 50,
+                        "date": "04/03/2022"
+                    },
+                    {
+                        key: 'W',
+                        value: 50,
+                        "date": "04/04/2022"
+                    },
+                    {
+                        key: 'T',
+                        value: 40,
+                        "date": "04/05/2022"
+                    },
+                    {
+                        key: 'F',
+                        value: 55,
+                        "date": "04/06/2022"
+                    },
+                    {
+                        key: 'S',
+                        value: 20,
+                        "date": "04/07/2022"
+                    }
+                ],
+                selected: true,
+                defaultSelected: "W"
+            },
+            {
+                "subTitle": "Apr 8 - Apr 14",
+                "startDate": "04/08/2022",
+                "endDate": "04/14/2022",
+                "graphData": [
+                    {
+                        key: 'M',
+                        value: 20,
+                        "date": "04/08/2022"
+                    },
+                    {
+                        key: 'T',
+                        value: 50,
+                        "date": "04/09/2022"
+                    },
+                    {
+                        key: 'W',
+                        value: 85,
+                        "date": "04/10/2022"
+                    },
+                    {
+                        key: 'T',
+                        value: 40,
+                        "date": "04/11/2022"
+                    },
+                    {
+                        key: 'F',
+                        value: 55,
+                        "date": "04/12/2022"
+                    },
+                    {
+                        key: 'S',
+                        value: 20,
+                        "date": "04/13/2022"
+                    },
+                    {
+                        key: 'S',
+                        value: 100,
+                        "date": "04/04/2022"
+                    }
+                ],
+                selected: false,
+                defaultSelected: "M"
+            }
+        ],
+        keys: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+        animation: true,
+        "total": 3
+    }
+}
